7aea4a284cfa7d8ba1854cb5070c84fb3854d84b
icapa <ivan.olea@gmail.com>
2016-10-25 22:55:47 +0200

    Crear tabla de Posts, sin cambiar el modo del scheme ni asignar autenticacion

--- api/readAllRecords.js
+++ api/readAllRecords.js
@@ -2,26 +2,27 @@
  * Created by icapa on 25/10/16.
  */
 var api = {
-    get: function (req,res,next){
-        // Chequear parametros
-        if (typeof req.params.length <0){
+    get: function (req, res, next) {
+
+        // chequear parametros
+        if (typeof req.params.length < 0) {
             return next();
         }
+
         var context = req.azureMobile;
         var user = context.user.id;
-        console.log('User: ', user);
+
         var query = {
-            sql: "Select * FROM Posts"
+            sql: "Select * FROM Autors"
         };
 
         req.azureMobile.data.execute(query)
-            .then(function(result){
+            .then(function (result) {
                 res.json(result);
             });
     }
+
 };
 
-/*api.get.access = 'anonymous';
-module.exports = api;*/
 api.get.access = 'authenticated';
 module.exports = api;
