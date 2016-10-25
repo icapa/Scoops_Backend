6a13d1f1c4c712ef0aedca868fad47dbe8d2c9e7
icapa <ivan.olea@gmail.com>
2016-10-25 21:53:58 +0200

    Crear tabla de Posts, sin cambiar el modo del scheme ni asignar autenticacion

---
+++ api/readAllRecords.js
@@ -0,0 +1,25 @@
+/**
+ * Created by icapa on 25/10/16.
+ */
+var api = {
+    get: function (req,res,next){
+        // Chequear parametros
+        if (typeof req.params.length <0){
+            return next();
+        }
+        var context = req.azureMobile;
+        var user = context.user.id;
+        console.log('User: ', user);
+        var query = {
+            sql: "Select * FROM Posts"
+        };
+        
+        req.azureMobile.data.execute(query)
+            .then(function(result){
+                res.json(result);
+            });
+    }
+};
+
+api.get.access = 'anonymous';
+module.exports = api;
--- tables/Posts.js
+++ tables/Posts.js
@@ -2,15 +2,49 @@
  * Created by icapa on 25/10/16.
  */
 
+/*
+ struct Post {
+
+ let title: String
+ let text: String
+ var photo: String
+ var latitude: Double
+ var longitude: Double
+ let author: String
+ var status: Bool
+ var rate: Double
+ var available: Bool
+ var containerName: String
+
+ }
+ */
+
+
 var azureMobileApps = require("azure-mobile-apps");
 
 var table = azureMobileApps.table();
 
 
 table.columns = {
-    "name" : "string",
-    "secondName" : "string"
+     "title": "String",
+     "text": "String",
+     "photo": "String",
+     "latitude": "Double",
+     "longitude": "Double",
+     "author": "String",
+     "status": "Bool",
+     "rate": "Double",
+     "available": "Bool",
+     "containerName": "String"
 };
+
+/*
+Permisos de acceso a la tabla
+ */
+
+table.read.access = 'anonymous';
+table.update.access = 'disabled';
+table.insert.access = 'anonymous';
 
 module.exports = table;
 
