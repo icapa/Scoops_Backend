e03074fc1776e6528becd793e362f4f468e40a29
icapa <ivan.olea@gmail.com>
2016-10-25 22:59:08 +0200

    Crear tabla de Posts, sin cambiar el modo del scheme ni asignar autenticacion

--- app.js
+++ app.js
@@ -5,14 +5,14 @@
 var express = require("express"),
     azuremobileapps = require("azure-mobile-apps");
 
-
 var app = express(),
     mobile = azuremobileapps();
 
-mobile.tables.add("./tables");
-mobile.api.add("./api");
+
+mobile.tables.import("./tables");
+
+mobile.api.import("./api");
 
 app.use(mobile);
 
 app.listen(process.env.PORT || 3000);
-
