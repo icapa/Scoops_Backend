00c82dbfa14b2d82a17c1ea0c8f7022d2b767b79
icapa <ivan.olea@gmail.com>
2016-10-25 18:43:35 +0200

    Version inicial del servicio

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by icapa on 25/10/16.
+ */
+
+var express = require("express"),
+    azuremobileapps = require("azure-mobile-apps");
+
+
+var app = express(),
+    mobile = azuremobileapps();
+
+mobile.table.add("./tables");
+mobile.api.add("./api");
+
+app.use(mobile);
+
+app.listen(process.env.PORT || 3000);
+
