3846ec81e251549f77a1ebbce3a900b9b948070a
icapa <ivan.olea@gmail.com>
2016-10-26 23:26:50 +0200

    Subo los cambios para express 4, y el POST

--- api/add.js
+++ api/add.js
@@ -5,27 +5,23 @@ var api = {
     post: function (req, res, next) {
 
         // chequear parametros
-        /*
-        if (typeof req.params.length < 0) {
-            return next();
-        }
-        */
+
+        var title = req.body.title;
+        var author = req.body.author;
+        var text = req.body.text;
+
 
         //var context = req.azureMobile;
         //var user = context.user.id;
-        /*
+
          var query = {
-         sql: "Select * FROM Autors"
+         sql: "INSERT INTO Posts VALUES("+title+","+author+","+text+")";
          };
 
          req.azureMobile.data.execute(query)
          .then(function (result) {
          res.json(result);
          });
-         */
-        /*let name = req.body.usuario*/
-        var name = req.body.usuario;
-        res.json({ "usuario" : name });
 
     }
 
