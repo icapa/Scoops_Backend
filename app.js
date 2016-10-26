8dbad97a5fc6a52f9a2184959d80cab6eae62a8e
icapa <ivan.olea@gmail.com>
2016-10-26 23:13:06 +0200

    Subo los cambios para express 4, y el POST

--- app.js
+++ app.js
@@ -10,14 +10,10 @@ var app = express(),
 
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({
     extended : true
 }));
-
-
-app.use(express.json());
-app.use(express.urlencoded());
 
 mobile.tables.import("./tables");
 
