4dfd00df3e13e6bea2711884d183b2fbf74463b4
icapa <ivan.olea@gmail.com>
2016-10-28 01:35:04 +0200

    Método PUT para modificar los posts

--- api/add.js
+++ api/add.js
@@ -13,50 +13,10 @@ var api = {
         var texto = req.query.text;
 
 
-
-
-        /*
-        console.log('---- LOS PARAMETROS -----');
-        console.log('Tituloooo :'+title);
-        console.log('Authorrrr:' + author);
-        console.log('Textxxxx:'+texto);
-        console.log('---- LOS PARAMETROS -----');
-        console.log('---- LOS PARAMETROS -----');
-        console.log('Tituloooo :'+title1);
-        console.log('Authorrrr:' + author1);
-        console.log('Textxxxx:'+texto1);
-        console.log('---- LOS PARAMETROS -----');
-        console.log('---- LOS PARAMETROS -----');
-        console.log('Tituloooo :'+title2);
-        console.log('Authorrrr:' + author2);
-        console.log('Textxxxx:'+texto2);
-        console.log('---- LOS PARAMETROS -----');
-        console.log('---- EL PUTO BODY ENTERO ----');
-        console.log(req.body);
-        console.log(typeof(req.body));
-        console.log(req.params.title);
-        console.log(req.param('title'));
-        */
-
         var context = req.azureMobile;
-
 
         var user = context.user.id;
 
-
-
-        /*
-         var query = {
-             sql: "INSERT INTO Posts (title) VALUES ('hola')"
-         };
-
-
-
-         req.azureMobile.data.execute(query)
-         .then(function (result) {
-            res.json(result);
-         });
-        */
         var item = {
             rate: 0.0,
             numRates : 0,
---
+++ api/updatepost.js
@@ -0,0 +1,47 @@
+/**
+ * Created by icapa on 28/10/16.
+ */
+var api = {
+    put: function (req, res, next) {
+
+
+
+        var id = req.query.id;
+        var titulo = req.query.title;
+        var texto = req.query.texto;
+        var autor = req.query.author;
+
+
+
+
+        var context = req.azureMobile;
+
+
+        var user = context.user.id;
+
+        var item = {
+            wantPublish : wantpublish
+        };
+
+
+        var query = {
+            sql: "UPDATE AuthorPosts SET title=@title, author=@author, text=@text WHERE id=@id",
+            parameters: [
+                {name: 'title' , value : titulo},
+                {name: 'author', value : autor},
+                {name: 'text', value:texto}
+            ]
+        };
+
+        req.azureMobile.data.execute(query)
+            .then(function (result) {
+                res.json({status: "OK"});
+            })
+            .catch(next)
+
+    }
+
+};
+
+api.put.access = 'authenticated';
+module.exports = api;
