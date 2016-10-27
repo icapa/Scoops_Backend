756fe9debadc05d21c3a9ac784f5edd9024b2825
icapa <ivan.olea@gmail.com>
2016-10-27 21:38:34 +0200

    Meto la lista de post del author

--- api/add.js
+++ api/add.js
@@ -78,5 +78,5 @@ var api = {
 
 };
 
-api.post.access = 'anonymous';
+api.post.access = 'authenticated';
 module.exports = api;
---
+++ api/authorposts.js
@@ -0,0 +1,38 @@
+/**
+ * Created by icapa on 27/10/16.
+ */
+var api = {
+    get: function (req, res, next) {
+
+        // chequear parametros
+        if (typeof req.params.length < 0) {
+            return next();
+        }
+        var publicada = req.params.publish
+        
+        var context = req.azureMobile;
+        //var user = context.user.id;
+        var user = 'sid:d88065d9dbcb66c21c4cc0de8ad05fe0';
+        console.log('------- GET PARMS --------');
+        console.log('Publicada: ' + publicada);
+        console.log('UserId:' + user);
+        
+         var query = {
+            sql: "Select * FROM AuthorPosts where userId='@userId'",
+            params: {userId : user}
+         };
+
+         req.azureMobile.data.execute(query)
+         .then(function (result) {
+            res.json(result);
+            })
+         .catch(next);
+
+
+
+    }
+
+};
+
+api.get.access = 'anonymous';
+module.exports = api;
