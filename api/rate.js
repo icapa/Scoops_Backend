c19aa942a86ab78dfc12ee4ece13f9bd0a98d724
icapa <ivan.olea@gmail.com>
2016-10-28 19:48:06 +0200

    Metiendo parte de valorar un post

--- api/rate.js
+++ api/rate.js
@@ -13,13 +13,13 @@ var api = {
 
 
         // Obtener un registro para sacar el numero de vallidaciones y la media
-        console.log("RATE: id:"+id);
-        console.log("RATE: rate:"+rate);
+        console.log("RATE: id:" + id);
+        console.log("RATE: rate:" + rate);
 
-        var query = {id:id};
+        var query = {id: id};
 
         context.tables('AuthorPosts')
-            .where (query)
+            .where(query)
             .read()
             .then(results => {
 
@@ -32,37 +32,37 @@ var api = {
                 var numero = sqlItem.numRates;
 
                 var valGlobal = valoracion * numero;
+                console.log("RATE: AHORA Val: " + valoracion);
+                console.log("RATE: AHORA numero:" + numero);
+                console.log("RATE: AHORA total:" + valGlobal);
 
                 valGlobal = valGlobal + rate;
                 numero = numero + 1;
-                valGlobal = valGlobal/numero;
+                valGlobal = valGlobal / numero;
 
+                console.log("RATE: LUEGO Val: " + valoracion);
+                console.log("RATE: LUEGO numero:" + numero);
+                console.log("RATE: LUEGO total:" + valGlobal);
 
                 console.log("RATE: Nueva valoracion global: " + valGlobal);
 
                 var query = {
                     sql: "UPDATE AuthorPosts SET rate=@rate, numRates=@numrates WHERE id=@id",
                     parameters: [
-                        {name: 'rate' , value : valGlobal},
-                        {name: 'numrates', value : numero},
+                        {name: 'rate', value: valGlobal},
+                        {name: 'numrates', value: numero},
                         {name: 'id', value: id}
                     ]
                 };
 
                 req.azureMobile.data.execute(query)
-                .then(function (result) {
-                    res.json({status: "OK", rate : valGlobal});
-                })
-                .catch(next)
+                    .then(function (result) {
+                        res.json({status: "OK"});
+                    })
+                    .catch(next)
 
             })
             .catch(next)
-
-
-
-
-
-
 
     }
 
